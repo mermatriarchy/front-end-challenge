cb1a7b7f24234b60b39feecbd252a94ce442713f
mermatriarchy <hailey.miller91@gmail.com>
2017-05-08 16:00:27 -0400

    author config test

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ $(document).ready(function($) {
   //utilize a circularly linked list to establish color sequence for click event
   //initialize node object
   //build linked list as a multidimensional array with initial 7 colors
-  //on click, move to next next in list, thereby changing the color of the div
+  //on click, move to next node in list, thereby changing the color of the div
   //on hover, show the data (color) value of the current node
 
 });//end wrapper function
