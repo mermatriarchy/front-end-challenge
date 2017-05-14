40b1d866cc85147a6f5efe30043f5e2ab2a2314e
mermatriarchy <hailey.miller91@gmail.com>
2017-05-13 22:08:31 -0400

    refactor/finish

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ $(document).ready(function($) {
   //check that jquery is loaded correctly
   console.log('ready!')
 
+  ////// TABLE OF CONTENTS /////
+  // lines 10-14: data structure
+  // lines 23-81: function definitions
+  // lines: function invocation
+
   //define color array
  var colorArray = [{color:'black', next:'blue'}, {color:'blue', next:'cyan'}, 
   {color:'cyan', next:'green'}, {color:'green', next:'magenta'}, {color:'magenta', next:'red'}, 
@@ -9,29 +14,40 @@ $(document).ready(function($) {
 
   //populate the color-well with divs on page load because
   //who wants to hand code 100 divs??
+  function populateColorWell(){
     for (i=0; i<100; i++){
       $('.color-well').append("<div class='color-box'></div>");
     }
+  }
+    
+  //helper function - increment indices on the colorArray
+  function nextIndex(colorIndex){
+    colorIndex++; 
+    //reset colorIndex variable when it gets to the end of the array indices
+    if (colorIndex === colorArray.length){
+        colorIndex = 0;
+        return colorIndex;
+      }    
+    return colorIndex; 
+  }
 
-    //fill in color of boxes
-    var colorIndex = 0;
-    $('.color-box').each(function(){
+  //fill in color of boxes
+  function colorFill(){
+      var colorIndex = 0;
+
+      $('.color-box').each(function(){
       //set background color and append hidden text with color name
       //add class to point back to the index for click function
-      
-      var color = colorArray[colorIndex]['color']
-      $(this).css('background-color', color)
-      .append("<p class='color-name arrayIndex-" + colorIndex +"'>" + color + "</p>")
+        var color = colorArray[colorIndex]['color']
+        $(this).css('background-color', color)
+        .append("<p class='color-name arrayIndex-" + colorIndex +"'>" + color + "</p>")
       //move to next array index on each box
-      colorIndex++;  
-      //reset colorIndex variable when it gets to the end of the array indices
-      if (colorIndex === colorArray.length){
-        colorIndex = 0;
-      }    
-    });
-
+        colorIndex = nextIndex(colorIndex);   
+      });
+  }
 
     //show color name on hover
+  function showColorNameOnHov(){
     $('.color-box').hover(function(){
       //show color name
       $(this).children().show();
@@ -41,32 +57,31 @@ $(document).ready(function($) {
         $(this).children().hide();
       }
     )
+  }
 
+    //cycle through the colors/colorArray on click
+  function nextColor(){
     $('.color-box').click(function(){
       //get the index of the color from the colorIndex class name
-      //var findColorIndex = $(this).children().attr('class').match(/\d+/)[0]
       //convert string to integer
       var colorIndex = parseInt($(this).children().attr('class').match(/\d+/)[0])
       //set new color to the value of next key in colorArray
       var nextColor = colorArray[colorIndex]['next']
       //increment colorIndex to match the index of the next item in colorArray
-      colorIndex++;  
-      //reset colorIndex variable when it gets to the end of the array indices
-      if (colorIndex === colorArray.length){
-        colorIndex = 0;
-      }    
+      colorIndex = nextIndex(colorIndex);  
       //remove element with current color to replace it with an element containing
       //the next color
       $(this).children().remove();
       //update background color and text on div
       //replace text & color index classname in p tag
       $(this).css('background-color', nextColor)
-      .append("<p class='color-name arrayIndex-" + colorIndex +"'>" + nextColor + "</p>");
+        .append("<p class='color-name arrayIndex-" + colorIndex +"'>" + nextColor + "</p>");
     });
-  //utilize a circularly linked list to establish color sequence for click event
-  //initialize node object
-  //build linked list as a multidimensional array with initial 7 colors
-  //on click, move to next node in list, thereby changing the color of the div
-  //on hover, show the data (color) value of the current node
+  }
+
+  populateColorWell();
+  colorFill();
+  showColorNameOnHov();
+  nextColor();
 
 });//end wrapper function
